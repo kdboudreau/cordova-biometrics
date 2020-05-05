0527cb7fc65905c86d1dfad5283653ad9805ddb7
Kathryn Boudreau <kdboudreau1@gmail.com>
2020-05-05 16:24:12 -0400

    adding for android

--- hello/platforms/android/app/src/main/assets/www/js/index.js
+++ hello/platforms/android/app/src/main/assets/www/js/index.js
@@ -20,33 +20,8 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    }
+    },
+    
+    
 };
 
-app.initialize();
-
-Fingerprint.isAvailable(isAvailableSuccess, isAvailableError);
-
-function isAvailableSuccess(result) {
-    /*
-    result depends on device and os. iPhone X will return 'face' other Android or iOS devices will return 'finger'  
-    */
-    alert("Fingerprint available");
-}
-
-function isAvailableError(error) {
-    // 'error' will be an object with an error code and message
-    alert(error.message);
-}
-
-Fingerprint.show({
-    description: "Some biometric description"
-}, successCallback, errorCallback);
-
-function successCallback(){
-    alert("Authentication successful");
-}
-
-function errorCallback(error){
-    alert("Authentication invalid " + error.message);
-}
