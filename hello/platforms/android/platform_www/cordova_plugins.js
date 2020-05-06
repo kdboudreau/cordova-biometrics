0e4db4654974360f10a3cc63aca9e6466e0e4d5d
Kathryn Boudreau <kdboudreau1@gmail.com>
2020-05-06 16:08:43 -0400

    more android changes

--- hello/platforms/android/platform_www/cordova_plugins.js
+++ hello/platforms/android/platform_www/cordova_plugins.js
@@ -3,6 +3,7 @@ module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+  "cordova-plugin-add-swift-support": "2.0.2",
   "cordova-plugin-whitelist": "1.3.4"
 };
 // BOTTOM OF METADATA
